bdcd1daad738e4bd33decc152da8b78123f76ef2
Joe Jiang <joejiang@joejiang-mlt.client.nvidia.com>
2024-10-17 14:19:07 +0800

    20241017_patch0

---
+++ codet/cli/codet.py
@@ -0,0 +1,43 @@
+# !/usr/bin/env python
+
+"""
+    codet command line interface
+"""
+
+HELLO_CODET = """
+=====================================================================================
+
+ ██████╗ ██████╗ ██████╗ ███████╗    ████████╗██████╗  █████╗ ██╗██╗     
+██╔════╝██╔═══██╗██╔══██╗██╔════╝    ╚══██╔══╝██╔══██╗██╔══██╗██║██║     
+██║     ██║   ██║██║  ██║█████╗         ██║   ██████╔╝███████║██║██║     
+██║     ██║   ██║██║  ██║██╔══╝         ██║   ██╔══██╗██╔══██║██║██║     
+╚██████╗╚██████╔╝██████╔╝███████╗       ██║   ██║  ██║██║  ██║██║███████╗
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝       ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝
+                                                                         
+=====================================================================================
+mit license
+
+copyright (c) 2024 clemente0620@gmail.com
+
+permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "software"), to deal
+in the software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the software, and to permit persons to whom the software is
+furnished to do so, subject to the following conditions:
+
+the above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the software.
+
+the software is provided "as is", without warranty of any kind, express or
+implied, including but not limited to the warranties of merchantability,
+fitness for a particular purpose and noninfringement.
+"""
+
+
+def main():
+    print(HELLO_CODET)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+def read_requirements(file_path):
+    # read requirements from file
+    # args: file_path (str): path to requirements file
+    # returns: list of requirement names
+    with open(file_path) as f:
+        requirements = f.read().splitlines()
+        print("pprobe install_requires:", requirements)
+        return requirements
+
+setup(
+    name="codet",
+    version="1.0.0",
+    description="A repository code trail tool",
+    long_description="A repository code trail tool",
+    url="https://www.codetrail.com/codetrail",
+    author="clemente0620",
+    author_email="clemente0620@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "codet=codet.cli.codet:main",
+        ]
+    },
+    packages=find_packages(exclude=["codet/tests"]),
+    include_package_data=True,
+    install_requires=read_requirements("./requirements.txt"),
+    zip_safe=False,
+)
