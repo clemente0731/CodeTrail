9de7df866098d8fd7fdcf9e4e9b6c87d69818879
clemente0620 <joejiang@joejiang-mlt.client.nvidia.com>
2024-10-17 17:11:11 +0800

    update git collect logic

--- codet/cli/codet.py
+++ codet/cli/codet.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from datetime import datetime, timedelta
 from collections import Counter
 from colorama import Fore, Style, init
+import argparse
 
 
 HELLO_CODET = r"""
@@ -25,7 +26,7 @@ HELLO_CODET = r"""
 init(autoreset=True)
 
 class GitAnalyzer:
-    def __init__(self, repo_path, days=7):
+    def __init__(self, repo_path, args):
         """
         Initializes the GitAnalyzer with the repository path and the number of days to analyze.
 
@@ -33,10 +34,17 @@ class GitAnalyzer:
         :param days: Number of days to look back for commits. Defaults to 7.
         """
         self.repo_path = repo_path
-        self.days = days
+        self.days = args.days
+        self.keyword = args.keyword
+        self.debug = args.debug
         # Open the Git repository at the specified path
         self.repo = git.Repo(repo_path)
         self.changed_files = []
+        # Stores commit records in the format {commit_id: {commit_message: commit message, 
+        # commit_date: commit date, commit_author: commit author, commit_email: 
+        # commit email, commit_diffs: commit diffs, commit_diff_count: number of changed files,
+        # commit_diff_summary: summary of diffs, commit_diff_files: list of changed files}}
+        self.commit_records = {}  
 
     def analyze_commits(self):
         since_date = datetime.now() - timedelta(days=self.days)
@@ -49,21 +57,70 @@ class GitAnalyzer:
             # Check if the commit has parent commits
             if commit.parents:
                 parent_commit = commit.parents[0]
-                print("Commit message summary:", commit.summary)
                 
-                # Get the diff index between the current commit and its parent
-                for diff in commit.diff(parent_commit, create_patch=True):
-                    print("---------------------------------------->")
-                    print(f"File: {diff.a_path}")
-                    # Print the file diff if it exists, otherwise indicate no changes
-                    print(f"File diff: {diff.diff.decode('utf-8') if diff.diff else 'N/A'}")
-                    print("----------------------------------------<")
+                commit_summary = commit.message.splitlines()[0]
+                commit_hexsha = commit.hexsha
+                commit_date = commit.committed_datetime
+                commit_author = commit.author.name 
+                commit_email = commit.author.email
+                commit_message = commit.message
+                commit_diffs = commit.diff(parent_commit, create_patch=True)
+                commit_diff_count = len(commit_diffs)
+                commit_diff_summary = f"{commit_diff_count} files changed"  # 简化差异摘要
+                commit_diff_files = [diff.a_path for diff in commit_diffs]
 
+
+                print(f"{Fore.BLUE}Commit Summary: {commit_summary:<50} (Commit ID: {commit_hexsha}){Style.RESET_ALL}")
+
+
+                if self.debug:
+                    print("---------------------------------------->")
+                    print(f"COMMIT ID: {commit_hexsha}")
+                    # print(f"COMMIT SUMMARY: {commit_summary}")
+                    # print(f"DATE: {commit_date}")
+                    # print(f"AUTHOR: {commit_author}")
+                    # print(f"EMAIL: {commit_email}")
+                    # print(f"DIFF COUNT: {commit_diff_count}")
+                    # print(f"DIFF SUMMARY: {commit_diff_summary}")
+                    # print(f"CHANGED FILES: {commit_diff_files}")
+                    
+                    for diff in commit_diffs:
+                        modified_content = diff.diff.decode('utf-8') if diff.diff else 'NO CHANGES'
+                        print(f"COMMIT MESSAGE: {commit_message}")
+                        print(f"COMMIT MODIFIED FILE NAME: {diff.a_path}")
+                        print(f"COMMIT MODIFIED CONTENT: {modified_content}")
+                        print("----------------------------------------<")
+                else:
+                    # this section gathers the diffs of the commit and checks for a keyword in the diffs.
+                    diffs_txt = ""
+                    for diff in commit_diffs:
+                        diffs_txt += f"COMMIT MESSAGE: {commit_message}"
+                        diffs_txt += f"COMMIT MODIFIED FILE NAME: {diff.a_path}"
+                        modified_content = diff.diff.decode('utf-8') if diff.diff else 'NO CHANGES'
+                        diffs_txt += f"COMMIT MODIFIED CONTENT: {modified_content}"
+    
+                    # # Check if a keyword is provided for searching in the diffs
+                    if self.keyword and self.keyword in diffs_txt:
+                        print(f"{Fore.RED} keyword '{self.keyword}' found in diff: {self.keyword}{Style.RESET_ALL}")
+                        self.commit_records[commit_hexsha] = {
+                            'commit_message': commit_message,
+                            'commit_date': commit_date,
+                            'commit_email': commit_email,
+                            'commit_diffs': diffs_txt.strip(),
+                        }
+                    elif not self.keyword:
+                        self.commit_records[commit_hexsha] = {
+                            'commit_message': commit_message,
+                            'commit_date': commit_date,
+                            'commit_email': commit_email,
+                            'commit_diffs': diffs_txt.strip(),
+                        }
+                
             # Get the diffs without creating a patch
             diffs = commit.diff(parent_commit, create_patch=False)
             for diff_item in diffs:
                 if diff_item.a_path:  # Ensure that the file path exists
-                    self.changed_files.append(diff_item.a_path)
+                    self.changed_files.append(diff_item.a_path)  # Count changed files for hotspot analysis
 
 
     def display_changes(self):
@@ -77,7 +134,7 @@ class GitAnalyzer:
         total_changes = sum(file_change_count.values())
 
         # Print the total number of changes
-        print(f"{Fore.GREEN}Total changes: {total_changes}{Style.RESET_ALL}")
+        print(f"\n{Fore.GREEN} Total hotspot changes detected: {total_changes}{Style.RESET_ALL}")
         
         for file, count in sorted_files:
             change_percentage = (count / total_changes) * 100
@@ -91,16 +148,16 @@ class GitAnalyzer:
 
             print(f"  {color}{count} {file}{Style.RESET_ALL}")
 
-import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Analyze commit history of a Git repository')
     parser.add_argument('-d', '--days', type=int, default=7, help='Number of days to look back for commits, defaults to 14 days')
-    parser.add_argument('-k', '--keyword', type=str, help='Search for a keyword in the current commit diff')
+    parser.add_argument('-k', '--keyword', type=str, default='', help='Search for a keyword in the current commit diff, default is empty')
+    parser.add_argument('--debug', action='store_true', default=False, help='Enable debug mode (default is off)')
     args = parser.parse_args()
     
     print(HELLO_CODET)
-    git_analyzer = GitAnalyzer("./", args.days)
+    git_analyzer = GitAnalyzer("./", args)
     git_analyzer.analyze_commits()
     git_analyzer.display_changes()
 
